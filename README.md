61143bd59134b4a4de14f493cf4d52f2c8620444
Antonflk55 <a.falk2106@gmail.com>
2025-02-22 10:55:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BTrade
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Antonflk55/BTrade)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
+<h1 className="text-3xl font-bold text-blue-500">Welcome to BTrade!</h1>;
+
